d5957e7a5e1d42d8b0509788c9da1df95cd4a8c2
Apprentice <apprentice@devbootcamp.com>
2013-09-08 14:10:44 -0700

    Added methods to users and skills

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,4 +4,10 @@ class Skill < ActiveRecord::Base
   has_many :users, through: :proficiencies
 
   validates :name, uniqueness: true 
+
+  def user_with_proficiency(number)
+    user_id = self.proficiencies.where(skill_level: number).first.user_id
+    User.find(user_id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,19 @@ class User < ActiveRecord::Base
   # Remember to create a migration!
   has_many :proficiencies
   has_many :skills, through: :proficiencies
+
+  def proficiency_for(skill)
+    user_id = self.id 
+    skill_id = skill.id
+    Proficiency.where(user_id: user_id, skill_id: skill_id).first.skill_level
+  end
+
+  def set_proficiency_for(skill, num)
+    user_id = self.id
+    skill_id = skill.id
+    skill_change = Proficiency.where(user_id: user_id, skill_id: skill_id).first
+    skill_change.skill_level = num
+    skill_change.save
+  end
+
 end
